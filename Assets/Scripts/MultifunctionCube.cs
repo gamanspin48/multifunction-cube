d4519dac441505d3e6c2d6425cf1a47b1f6ad9f5
Teguh Ikhlas <teguh@adhivasindo.co.id>
2020-08-02 13:27:38 +0700

    duplicate

---
+++ Assets/Scripts/MultifunctionCube.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultifunctionCube : MonoBehaviour
+{   
+    public enum Functions{ Duplicate, Add}
+
+    public Functions function;
+    public Transform prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (function)
+        {
+            case Functions.Duplicate:
+                this.gameObject.AddComponent<Lean.Touch.LeanDragTranslate>();
+                break;
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {   
+        Debug.Log("collide");
+        switch (function)
+        {   
+            case Functions.Duplicate:
+                Transform g = Instantiate(collision.gameObject.transform, new Vector3(2 * 2.0F, 0, 0), Quaternion.identity);
+                break;
+            
+        }
+    }
+
+}
