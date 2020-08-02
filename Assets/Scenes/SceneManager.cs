9de8ae12485ebe0d84952a3e1322b7f8356c0717
Teguh Ikhlas <teguh@adhivasindo.co.id>
2020-08-02 15:12:00 +0700

    add and rotation

---
+++ Assets/Scenes/SceneManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject mainCube;
+    GameObject g;
+    public Text btnText;
+    public Text btnRotateText;
+    bool isAdd;
+    bool isRotateLeft = true;
+
+    public void RotateCube(){
+        if(isRotateLeft)
+            RotateRight();
+        else
+            RotateLeft();
+    }
+
+    private void RotateLeft(){
+        btnRotateText.text = "Rotate Right";
+        isRotateLeft = true;
+        g.transform.Rotate(0,50f,0,Space.Self);
+    }
+
+    private void RotateRight(){
+        btnRotateText.text = "Rotate Left";
+        isRotateLeft = false;
+        g.transform.Rotate(0,-50f,0,Space.Self);
+    }
+
+    public void AddRemoveMainCube(){
+        if(isAdd){
+            RemoveMainCube();
+        }else{
+            AddMainCube();
+        }
+    }
+
+    private void AddMainCube(){
+        isAdd = true;
+        btnText.text = "Remove";
+        g = Instantiate(mainCube, new Vector3(0, 0, 0), Quaternion.identity).gameObject;
+        g.transform.Rotate(0,25f,0,Space.Self);
+    }
+
+    private void RemoveMainCube(){
+        isAdd = false;
+        btnText.text = "Add";
+        Destroy(g);
+    }
+
+}
--- Assets/Scripts/MultifunctionCube.cs
+++ Assets/Scripts/MultifunctionCube.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class MultifunctionCube : MonoBehaviour
 {   
-    public enum Functions{ Duplicate, Add}
-
-    public Functions function;
-    public Transform prefab;
+    [Header("Selection")]
+    public bool fullSelect;
+    [Header("Convert")]
+    public bool meshVertices;
+    [Header("Handling")]
+    public bool duplicate;
+    // [Header("Modify")]
+    // [Header("Splines")]
+    // [Header("Tags")]
+    // [Header("UI")]
+    
+ 
     // Start is called before the first frame update
     void Start()
     {
-        switch (function)
-        {
-            case Functions.Duplicate:
-                this.gameObject.AddComponent<Lean.Touch.LeanDragTranslate>();
-                break;
-            
-        }
+      
     }
 
     // Update is called once per frame
@@ -26,15 +28,11 @@ public class MultifunctionCube : MonoBehaviour
         
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {   
         Debug.Log("collide");
-        switch (function)
-        {   
-            case Functions.Duplicate:
-                Transform g = Instantiate(collision.gameObject.transform, new Vector3(2 * 2.0F, 0, 0), Quaternion.identity);
-                break;
-            
+        if (duplicate){
+            Transform g = Instantiate(other.gameObject.transform, new Vector3(2 * 2.0F, 0, 0), Quaternion.identity);
         }
     }
 
